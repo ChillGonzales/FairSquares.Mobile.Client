0dbbd85fda18e207e3a1a0c85236fc130751e620
ChillGonzales <cmmonroe2010@gmail.com>
2018-12-17 21:48:27 -0500

    basic push notifications working

--- MobileClient/MobileClient.Android/MainActivity.cs
+++ MobileClient/MobileClient.Android/MainActivity.cs
@@ -10,12 +10,18 @@ using MobileClient.Authentication;
 using Android.Content;
 using Plugin.InAppBilling;
 using Plugin.CurrentActivity;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Iid;
 
 namespace MobileClient.Droid
 {
     [Activity(Label = "Fair Squares", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -23,6 +29,9 @@ namespace MobileClient.Droid
 
             base.OnCreate(savedInstanceState);
 
+            CreateNotificationChannel();
+            IsPlayServicesAvailable();
+            Log.Error("CustomApp", $"Token: '{FirebaseInstanceId.Instance.Token.ToString()}'");
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, savedInstanceState);
             CrossCurrentActivity.Current.Init(this, savedInstanceState);
@@ -34,6 +43,47 @@ namespace MobileClient.Droid
         {
             base.OnActivityResult(requestCode, resultCode, data);
             InAppBillingImplementation.HandleActivityResult(requestCode, resultCode, data);
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification 
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID, "FCM Notifications", NotificationImportance.Default)
+            {
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            var resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    Log.Debug("push", GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                }
+                else
+                {
+                    Log.Debug("push", "This device is not supported");
+                    Finish();
+                }
+
+                return false;
+            }
+
+            Log.Debug("push", "Google Play Services is available.");
+            return true;
         }
     }
 }
