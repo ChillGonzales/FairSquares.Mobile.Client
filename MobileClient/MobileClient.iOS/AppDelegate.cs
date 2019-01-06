2d6d00398654433fd31996ed5cae592a8579eae5
ChillGonzales <cmmonroe2010@gmail.com>
2019-01-05 20:01:30 -0500

    updates for ios push notifications

--- MobileClient/MobileClient.iOS/AppDelegate.cs
+++ MobileClient/MobileClient.iOS/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Firebase.CloudMessaging;
 using Foundation;
 using UIKit;
+using UserNotifications;
 using Xamarin.Auth;
 
 namespace MobileClient.iOS
@@ -12,7 +13,7 @@ namespace MobileClient.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -27,8 +28,64 @@ namespace MobileClient.iOS
             global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
             LoadApplication(new App());
 
+            // get permission for notification
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+            }
+            else
+            {
+                // iOS 9 <=
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            // Firebase component initialize
+            Firebase.Core.App.Configure();
+
+            Messaging.SharedInstance.Delegate = this;
+
+            Firebase.InstanceID.InstanceId.Notifications.ObserveTokenRefresh((sender, e) => {
+                var newToken = Firebase.InstanceID.InstanceId.SharedInstance.Token;
+                // if you want to send notification per user, use this token
+                System.Diagnostics.Debug.WriteLine(newToken);
+            });
+
+            // Register your app for remote notifications.
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
             return base.FinishedLaunching(app, options);
         }
+
         public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
         {
             // Convert NSUrl to Uri
@@ -38,6 +95,65 @@ namespace MobileClient.iOS
             App.Container.GetInstance<OAuth2Authenticator>().OnPageLoading(uri);
 
             return true;
+        }
+
+        public override void DidEnterBackground(UIApplication uiApplication)
+        {
+            Messaging.SharedInstance.Disconnect();
+        }
+
+        public override void OnActivated(UIApplication uiApplication)
+        {
+            base.OnActivated(uiApplication);
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Messaging.SharedInstance.ApnsToken = deviceToken;
+        }
+
+        // iOS 9 <=, fire when recieve notification foreground
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            Messaging.SharedInstance.AppDidReceiveMessage(userInfo);
+
+            // Generate custom event
+            NSString[] keys = { new NSString("Event_type") };
+            NSObject[] values = { new NSString("Recieve_Notification") };
+            var parameters = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(keys, values, keys.Length);
+
+            if (application.ApplicationState == UIApplicationState.Active)
+            {
+                System.Diagnostics.Debug.WriteLine(userInfo);
+                var aps_d = userInfo["aps"] as NSDictionary;
+                var alert_d = aps_d["alert"] as NSDictionary;
+                var body = alert_d["body"] as NSString;
+                var title = alert_d["title"] as NSString;
+                debugAlert(title, body);
+            }
+        }
+
+        // iOS 10, fire when recieve notification foreground
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            var title = notification.Request.Content.Title;
+            var body = notification.Request.Content.Body;
+            debugAlert(title, body);
+        }
+
+        private void ConnectFCM()
+        {
+            Messaging.SharedInstance.Connect((error) =>
+            {
+                System.Diagnostics.Debug.WriteLine(error != null ? "error occured" : "connect success");
+            });
+        }
+
+        private void debugAlert(string title, string message)
+        {
+            var alert = new UIAlertView(title ?? "Title", message ?? "Message", null, "Cancel", "OK");
+            alert.Show();
         }
     }
 }
---
+++ MobileClient/MobileClient.iOS/Utilities/MessagingSubscriber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Firebase.CloudMessaging;
+using Foundation;
+using MobileClient.iOS.Utilities;
+using MobileClient.Utilities;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MessagingSubscriber))]
+namespace MobileClient.iOS.Utilities
+{
+    public class MessagingSubscriber : IMessagingSubscriber
+    {
+        public void Subscribe(List<string> topics)
+        {
+            if (topics == null || !topics.Any())
+                return;
+            foreach (var topic in topics)
+                Messaging.SharedInstance.Subscribe($"/topics/{topic}");
+        }
+
+        public void Unsubscribe(List<string> topics)
+        {
+            if (topics == null || !topics.Any())
+                return;
+            foreach (var topic in topics)
+                Messaging.SharedInstance.Unsubscribe($"/topics/{topic}");
+        }
+    }
+}
