1cfa83ad2821e8a89251ef46d1ac2365ecb04d37
ChillGonzales <cmmonroe2010@gmail.com>
2019-06-06 21:33:12 -0400

    base tab

--- MobileClient/MobileClient/Routes/BaseTab.xaml.cs
+++ MobileClient/MobileClient/Routes/BaseTab.xaml.cs
@@ -1,20 +1,43 @@
-﻿using System;
+﻿using MobileClient.Models;
+using MobileClient.Utilities;
+using MobileClient.Utility;
+using MobileClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MobileClient.Routes
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BaseTab : ContentPage
+    public partial class BaseTab : TabbedPage
     {
+        private readonly ICommand _onAppearing;
+
         public BaseTab()
         {
             InitializeComponent();
+            var vm = new BaseTabViewModel(App.Container.GetInstance<ICache<SettingsModel>>(),
+                                          App.Container.GetInstance<IPageFactory>(),
+                                          this.Navigation,
+                                          new Dictionary<BaseNavPageType, Page>()
+                                          {
+                                              { BaseNavPageType.Account, AccountTab },
+                                              { BaseNavPageType.MyOrders, MyOrdersTab },
+                                              { BaseNavPageType.Order, OrderTab }
+                                          });
+            _onAppearing = vm.OnAppearingBehavior;
+            BindingContext = vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _onAppearing.Execute(null);
         }
     }
 }
--- MobileClient/MobileClient/Routes/BaseTabViewModel.cs
+++ MobileClient/MobileClient/Routes/BaseTabViewModel.cs
@@ -1,10 +1,80 @@
-﻿using System;
+﻿using MobileClient.Models;
+using MobileClient.Utilities;
+using MobileClient.Utility;
+using MobileClient.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MobileClient.Routes
 {
-    public class BaseTabViewModel
+    public class BaseTabViewModel : INotifyPropertyChanged
     {
+        public readonly ICommand OnAppearingBehavior;
+        private readonly ICache<SettingsModel> _settings;
+        private readonly IPageFactory _pageFactory;
+        private readonly INavigation _nav;
+        private readonly IDictionary<BaseNavPageType, Page> _pages;
+        private bool _dialogShown;
+        private Page _currentPage;
+
+        public BaseTabViewModel(ICache<SettingsModel> settings,
+                                IPageFactory pageFactory,
+                                INavigation nav,
+                                IDictionary<BaseNavPageType, Page> pages)
+        {
+            _settings = settings;
+            _pageFactory = pageFactory;
+            _nav = nav;
+            _pages = pages;
+            OnAppearingBehavior = new Command(async () =>
+            {
+                if (!_dialogShown && (!_settings.GetAll().Any() || _settings.Get("").DisplayWelcomeMessage))
+                {
+                    _dialogShown = true;
+                    await _nav.PushAsync(_pageFactory.GetPage(PageType.Instruction, true));
+                }
+            });
+            NavigateFromMenu(BaseNavPageType.MyOrders);
+        }
+
+
+        public void NavigateFromMenu(BaseNavPageType pageType)
+        {
+            switch (pageType)
+            {
+                case BaseNavPageType.Account:
+                    CurrentPage = _pages[BaseNavPageType.Account];
+                    break;
+                case BaseNavPageType.MyOrders:
+                    CurrentPage = _pages[BaseNavPageType.MyOrders];
+                    break;
+                case BaseNavPageType.Order:
+                    CurrentPage = _pages[BaseNavPageType.Order];
+                    break;
+                default:
+                    CurrentPage = _pages[BaseNavPageType.MyOrders];
+                    break;
+            }
+        }
+
+        // Bound properties
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Page CurrentPage
+        {
+            get
+            {
+                return _currentPage;
+            }
+            set
+            {
+                _currentPage = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPage)));
+            }
+        }
     }
 }
