dede3679b340ef87efc5d0d4c06838102b41a37f
ChillGonzales <cmmonroe2010@gmail.com>
2019-06-08 15:08:15 -0400

    bugfix for launch

--- MobileClient/MobileClient/Routes/BaseTab.xaml.cs
+++ MobileClient/MobileClient/Routes/BaseTab.xaml.cs
@@ -24,7 +24,7 @@ namespace MobileClient.Routes
             CurrentPage = MyOrdersTab;
             var vm = new BaseTabViewModel(App.Container.GetInstance<ICache<SettingsModel>>(),
                                           App.Container.GetInstance<IPageFactory>(),
-                                          this.Navigation);
+                                          CurrentPage.Navigation);
             _onAppearing = vm.OnAppearingBehavior;
             BindingContext = vm;
         }
