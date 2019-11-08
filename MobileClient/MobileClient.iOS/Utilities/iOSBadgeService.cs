5d289a7add243ea0e0131e27c344c52b648d5a78
ChillGonzales <cmmonroe2010@gmail.com>
2019-11-07 19:15:10 -0500

    adding badge service

---
+++ MobileClient/MobileClient.iOS/Utilities/iOSBadgeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using MobileClient.iOS.Utilities;
+using MobileClient.Services;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(iOSBadgeService))]
+namespace MobileClient.iOS.Utilities
+{
+    public class iOSBadgeService : IBadgeService
+    {
+    }
+}
